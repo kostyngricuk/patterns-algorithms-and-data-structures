987b1a5855b10434c91d17bbb4c2c0a5389ce8b2
Kanstantsin Hrytsuk <kanstantsin.hrytsuk@instinctools.com>
2025-10-15 06:40:13 +0300

    fix: update project name in Docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -23,7 +23,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'kostyngricuk', // Usually your GitHub org/user name.
-  projectName: 'paads', // Usually your repo name.
+  projectName: 'patterns-algorithms-and-data-structures', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
