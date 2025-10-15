0966bf4afee81a6bbda5809feacbc16ff724f428
Kanstantsin Hrytsuk <kanstantsin.hrytsuk@instinctools.com>
2025-10-15 06:43:26 +0300

    fix: update baseUrl for GitHub pages deployment

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   url: 'https://paads.kostyngricuk.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/patterns-algorithms-and-data-structures/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
